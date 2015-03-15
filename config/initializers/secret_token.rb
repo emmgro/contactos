c74fcd8724be9b8b2057a84bd67198eeb47be542
Emmanuelle Guerrero <emmaguerreros@gmail.com>
2015-03-14 21:20:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contactos::Application.config.secret_key_base = 'fb4e6edfa6581b77446ab7abcadef3dd5435e86562ec8aebe0caeb7af057b17f8337ae868a069450366041176517999f35dd433f20a2273b25b4d439cda59993'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Contactos::Application.config.session_store :cookie_store, key: '_contactos_session'
